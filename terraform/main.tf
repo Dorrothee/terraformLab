48a78fb6a082b1df41bd07988cd34a0e87042ea7
Dorrothee <112761090+Dorrothee@users.noreply.github.com>
2023-12-21 01:15:15 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,109 +1,126 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
+#terraform {
+#  required_version = ">=0.13.0"
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 3.0"
+#    }
+#  }
+#}
 
-resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "eu-central-1"  # replace with your AWS region
-}
+# Configure the AWS provider
+#provider "aws" {
+#  region     = "eu-central-1"
+#}
 
-resource "aws_subnet" "subnet2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1"  # replace with your AWS region
-}
+#resource "aws_security_group" "web_app" {
+#  name        = "web_app"
+#  description = "security group"
+#  ingress {
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-data "aws_security_group" "existing" {
-  id = "sg-022b28d8ed136c25a"
-}
+# ingress {
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
+#  egress {
+#    from_port   = 0
+#    to_port     = 65535
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-resource "aws_default_route_table" "main" {
-  default_route_table_id = aws_vpc.main.default_route_table_id
+#  tags= {
+#    Name = "web_app"
+#  }
+#}
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+#resource "aws_instance" "webapp_instance" {
+#  ami           = "ami-0669b163befffbdfc"
+#  instance_type = "t2.micro"
+#  security_groups= ["web_app"]
+#  tags = {
+#    Name = "webapp_instance"
+#  }
+#}
 
-  tags = {
-    Name = "main"
-  }
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-
-
+#output "instance_public_ip" {
+#  value     = aws_instance.webapp_instance.public_ip
+#  sensitive = true
+#}
 
 
-provider "aws" {
-  region = "eu-central-1"
-}
 
-resource "aws_ecs_cluster" "my_cluster" {
-  name = "my_cluster"
-}
 
-resource "aws_ecs_service" "my_service" {
-  name            = "my_service"
-  cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.my_task_definition.arn
-  desired_count   = 1
-  launch_type     = "FARGATE"  // Or "EC2" depending on your preference
 
-  network_configuration {
-    subnets = ["subnet1", "subnet2"]
-    security_groups = [aws_security_group.allow_all.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = "${aws_lb_target_group.my_target_group.arn}"
-    container_name   = "my_container"
-    container_port   = 8080
-  }
-}
-
-resource "aws_ecs_task_definition" "my_task_definition" {
-
-  family                   = "my_task_definition"
-  network_mode             = "awsvpc"   //required if FARGATE launch type is selected
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"     // adjust to your needs
-  memory                   = "512"     // adjust to your needs
-  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
-
-  container_definitions = jsonencode([{
-    name   = "my_container"
-    image  = "504306331314.dkr.ecr.eu-central-1.amazonaws.com/maven_app_repo:latest"
-    cpu    = 0
-    memory = 512
-
-    portMappings = [{
-      containerPort = 8080
-      protocol      = "tcp"
-    }]
-  }])
-}
-
-resource "aws_iam_role" "ecs_execution_role" {
-  name               = "ecs_execution_role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_assume_role.json
-}
-
-data "aws_iam_policy_document" "ecs_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.28.0"
     }
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
+# Configure the AWS provider
+provider "aws" {
+  region = "eu-central-1"
+  #region = "us-east-1"
+}
+
+variable "REPOSITORY_URI" {
+  type = string
+}
+
+resource "aws_lightsail_container_service" "maven_application" {
+  name = "maven-app-static"
+  power = "nano"
+  scale = 1
+
+  private_registry_access {
+    ecr_image_puller_role {
+      is_active = true
+    }
+  }
+
+
+  tags = {
+    version = "1.0.0"
+  }
+}
+
+resource "aws_lightsail_container_service_deployment_version" "maven_app_deployment" {
+  container {
+    container_name = "maven-application"
+
+    image = "${var.REPOSITORY_URI}:latest"
+
+    ports = {
+      # Consistent with the port exposed by the Dockerfile and app.py
+      8080 = "HTTP"
+    }
+  }
+
+  public_endpoint {
+    container_name = "maven-application"
+    # Consistent with the port exposed by the Dockerfile and app.py
+    container_port = 8080
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout_seconds     = 2
+      interval_seconds    = 5
+      path                = "/"
+      success_codes       = "200-499"
+    }
+  }
+
+  service_name = aws_lightsail_container_service.maven_application.name
 }
